0caa12cf26ba0e0bf7feb9a2918cf1b391dfb82f
pefi1011 <pefi1011@gmail.com>
2014-12-07 22:52:31 +0100

    code cleanup

--- client/views/item/item_entry.js
+++ client/views/item/item_entry.js
@@ -3,16 +3,4 @@ Template.itemEntry.helpers({
     return this.userId == Meteor.userId();
   },
 
-  itemPhoto1: function() {
-    // var photos = this.itemPhotos;
-    // if (photos && photos[0]){
-    //   return photos[0];
-    // }
-    // return null;
-
-    var photo = Pictures.findOne().data;
-    return photo;
-
-  }
-
 });
