74487d4af32854608c92b4019e93fddf673f52ec
Benjamin Malik <benjamin.malik@gmail.com>
2014-12-09 17:05:24 +0100

    added rounded distance

--- client/views/item/item_entry.js
+++ client/views/item/item_entry.js
@@ -5,12 +5,15 @@ Template.itemEntry.helpers({
 
   distance: function(){
     var itemLocation = new google.maps.LatLng(this.location.lat, this.location.lng);
-
     var currentLocation = new google.maps.LatLng(Geolocation.latLng().lat, Geolocation.latLng().lng);
-
     var dist = google.maps.geometry.spherical.computeDistanceBetween (itemLocation, currentLocation) / 1000;
+    var distRounded = round(dist, 0)
     debugger
-    return dist;
+    if (distRounded < 1) {
+      return "< 1";
+    }
+    return distRounded;
+
   }
 
 });
