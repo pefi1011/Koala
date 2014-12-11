a11e668bfdaae8c3b2c517072eec627495b39e12
pefi1011 <pefi1011@gmail.com>
2014-12-11 23:20:50 +0100

    code cleanup

--- client/views/item/items_list.js
+++ client/views/item/items_list.js
@@ -1,7 +1,2 @@
 Template.itemsList.helpers({
-  /*hasMoreItems: function(){
-    this.items.rewind();
-    var hasMore = Router.current().itemsLimit() == this.items.fetch().length;
-    return hasMore;
-  }*/
 });
