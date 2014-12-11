d56275b449ea38af14881bcdf40f696bee0d59bf
Vassil Dimov <vsldimov@gmail.com>
2014-12-11 23:13:04 +0100

    FILIP: smoother pagination

--- client/views/item/item_list.js
+++
@@ -1,3 +0,0 @@
-Template.itemsList.helpers({
-  
-});
--- client/views/item/items_list.js
+++ client/views/item/items_list.js
@@ -1,7 +1,7 @@
 Template.itemsList.helpers({
-  hasMoreItems: function(){
+  /*hasMoreItems: function(){
     this.items.rewind();
     var hasMore = Router.current().itemsLimit() == this.items.fetch().length;
     return hasMore;
-  }
+  }*/
 });
