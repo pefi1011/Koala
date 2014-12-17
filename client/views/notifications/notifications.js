fe473a8632c8bf215634ee9659aae8f8d68d4e04
pefi1011 <pefi1011@gmail.com>
2014-12-17 21:57:17 +0100

    #20 at least one photo is required

--- client/views/notifications/notifications.js
+++ client/views/notifications/notifications.js
@@ -21,11 +21,15 @@ Template.notification.events({
 
 
 Meteor.startup(function(){
+
+  // vibrate for 500 when a notification is added
   Notifications.find({}).observe({
+
     added:function (doc) {
       if (Meteor.isCordova){
         navigator.notification.vibrate(500);
       }
     }
+
   });
 });
