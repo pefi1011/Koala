61f7e8dacc99157a301ce525fa644dbc8efdd779
Vassil Dimov <vsldimov@gmail.com>
2014-12-20 12:23:01 +0100

    Gift ion added to the header.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,4 @@
+// Items
 Meteor.publish('items', function(options) {
   return Items.find({}, options);
 });
@@ -6,14 +7,19 @@ Meteor.publish('singleItem', function(id) {
   return id && Items.find(id);
 });
 
+
+// Comments
 Meteor.publish('comments', function(itemId) {
   return Comments.find({itemId: itemId});
 });
 
+
+// Notifications
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId});
 });
 
+// Photos
 Meteor.publish('photos', function(itemId) {
   return Photos.find({itemId: itemId});
 });
