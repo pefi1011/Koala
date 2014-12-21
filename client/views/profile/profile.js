1dae617475b64403c2e67281c0465135d93388aa
Benjamin Malik <benjamin.malik@gmail.com>
2014-12-21 13:56:37 +0100

    changed css of user profile changed rating implementation based on db values

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -11,6 +11,28 @@ Template.profile.helpers({
     return this.createdAt;
   },
 
+  currentlyPosted: function() {
+    return "myItems count";
+  },
 
+  firstStar: function() {
+    return this.profile.rating > 0;
+  },
+
+  secondStar: function() {
+    return this.profile.rating > 1;
+  },
+
+  thirdStar: function() {
+    return this.profile.rating > 2;
+  },
+
+  fourthStar: function() {
+    return this.profile.rating > 3;
+  },
+
+  fithStar: function() {
+    return this.profile.rating > 4;
+  }
 
 });
