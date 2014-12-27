3e22c3fab64af565da04e8307154fff14ced47d2
pefi1011 <pefi1011@gmail.com>
2014-12-27 21:59:51 +0100

    - updated raix:push - wrap up sending push and regular notifications

--- client/init.js
+++ client/init.js
@@ -1,4 +1,13 @@
+
 Meteor.startup(function(){
+
+  // Fire up push asap
+  Push.init({
+    gcm: {
+      projectNumber: '420138116229'
+    }
+  });
+
 
   // vibrate for 500 when a notification is added
   Notifications.find({}).observe({
@@ -11,3 +20,13 @@ Meteor.startup(function(){
 
   });
 });
+//
+// Push.send({
+//   from: 'Koala',
+//   title: 'Interesse geäußert',
+//   text: 'Filip Perisic interessiert sich für Ihren "Schrank" Artikel',
+//   count: 2,
+//   query: {
+//     userId: 'pKt8Qj2HBH7MmnCsF'
+//   }
+// });
---
+++ server/pushServer.js
@@ -0,0 +1,27 @@
+// explicitly allow users to send push notifications from client-side.
+Push.allow({
+  send: function(userId, notification) {
+    // Allow all users to send to everybody - For test only!
+    return true;
+  }
+});
+
+
+var optionsProduction = {
+  gcm: {
+    apiKey: 'AIzaSyDxn-_M3CXtqY1dj7XC9VGq_23QsXQxIh8'
+  },
+  // apn: {
+  //   'passphrase': 'xxxxxxx',
+  //   // Place the certificate files in /private
+  //   'certData': Assets.getText('apnProdCert.pem'),
+  //   'keyData': Assets.getText('apnProdKey.pem'),
+  // },
+  production: true
+};
+
+//This will log details about whats going on in the system.
+Push.debug = true;
+
+// Fire up the push notification server
+Push.init(optionsProduction);
