ada70c9b629357fb85018c450c8e11a0b4432d02
pefi1011 <pefi1011@gmail.com>
2014-12-27 22:10:46 +0100

    removed vibration when adding into notifications

--- client/init.js
+++ client/init.js
@@ -1,4 +1,3 @@
-
 Meteor.startup(function(){
 
   // Fire up push asap
@@ -8,25 +7,4 @@ Meteor.startup(function(){
     }
   });
 
-
-  // vibrate for 500 when a notification is added
-  Notifications.find({}).observe({
-
-    added:function (doc) {
-      if (Meteor.isCordova){
-        navigator.notification.vibrate(500);
-      }
-    }
-
-  });
 });
-//
-// Push.send({
-//   from: 'Koala',
-//   title: 'Interesse geäußert',
-//   text: 'Filip Perisic interessiert sich für Ihren "Schrank" Artikel',
-//   count: 2,
-//   query: {
-//     userId: 'pKt8Qj2HBH7MmnCsF'
-//   }
-// });
