3aca1460d23626a1329e8b8445dd6f6235a9fbc1
pefi1011 <pefi1011@gmail.com>
2014-12-29 17:32:24 +0100

    raix:push update

--- client/init.js
+++ client/init.js
@@ -1,10 +1,4 @@
 Meteor.startup(function(){
 
-  //Fire up push asap
-  Push.init({
-    gcm: {
-      projectNumber: '420138116229' // probably is this the way how gcm pairs app and server
-    }
-  });
 
 });
--- server/pushServer.js
+++ server/pushServer.js
@@ -1,3 +1,14 @@
+// // When messages opens the app:
+// Push.addListener('startup', function(notification) {
+//   console.log('Payload', notification.payload);
+// });
+//
+// // When messages arrives in app already open:
+// Push.addListener('message', function(notification) {
+//   console.log('Payload', notification.payload);
+// });
+
+
 // explicitly allow users to send push notifications from client-side.
 Push.allow({
   send: function(userId, notification) {
@@ -7,22 +18,4 @@ Push.allow({
 });
 
 
-var optionsProduction = {
-  gcm: {
-    apiKey: 'AIzaSyDxn-_M3CXtqY1dj7XC9VGq_23QsXQxIh8',
-    projectNumber: '420138116229'
-  },
-  // apn: {
-  //   'passphrase': 'xxxxxxx',
-  //   // Place the certificate files in /private
-  //   'certData': Assets.getText('apnProdCert.pem'),
-  //   'keyData': Assets.getText('apnProdKey.pem'),
-  // },
-  production: true
-};
-
-//This will log details about whats going on in the system.
 Push.debug = true;
-
-// Fire up the push notification server
-Push.init(optionsProduction);
