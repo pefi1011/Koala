d7fb0e0e8d6d6ba56b9a3e9869e0eb847e4fd89d
pefi1011 <pefi1011@gmail.com>
2014-12-29 20:53:20 +0100

    #37
    opening item page if push notification has been sent because someone
    commented an item

---
+++ client/templates/notifications/pushNotifications.js
@@ -0,0 +1,34 @@
+//When messages opens the app:
+Push.addListener('startup', function(notification) {
+
+  // notification has been sent because someone commented an item
+  if(notification.payload.pushType === 'comment'){
+
+    // go to items page because there are the comments
+    Router.go('itemPage', {
+      _id: notification.payload.itemId
+    });
+  }
+  else if(notification.payload.pushType === 'interest'){
+
+    //TODO implement
+    alert('not implemented yet');
+  }
+});
+
+
+// // When messages arrives in app already open:
+Push.addListener('message', function(notification) {
+
+
+  // Router.go('itemPage', {
+  //   _id: notification.payload.itemId
+  // });
+
+  // if(notification.payload.title === 'Neuer Kommentar'){
+  //
+  //   Router.go('itemPage', {
+  //     _id: id
+  //   });
+  // }
+});
--- server/pushServer.js
+++ server/pushServer.js
@@ -1,12 +1,4 @@
-// // When messages opens the app:
-// Push.addListener('startup', function(notification) {
-//   console.log('Payload', notification.payload);
-// });
-//
-// // When messages arrives in app already open:
-// Push.addListener('message', function(notification) {
-//   console.log('Payload', notification.payload);
-// });
+
 
 
 // explicitly allow users to send push notifications from client-side.
